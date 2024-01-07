67df649cf8da2415b62361f4cb4714ef52260b8c
Saba Zedginidze <saba.2003.sz@gmail.com>
2024-01-07 19:08:20 +0400

    Add project files

---
+++ src/main/java/com/mziuri/Application.java
@@ -0,0 +1,38 @@
+package com.mziuri;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
+
+public class Application {
+
+    public static void main(String[] args) throws LifecycleException {
+
+        Tomcat tomcat = new Tomcat();
+
+        tomcat.enableNaming();
+        tomcat.setPort(8989);
+        tomcat.getConnector();
+
+        String ctxPath = "/messenger";
+        String webappDir = new File("src/main/webapp").getAbsolutePath();
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp(ctxPath, webappDir);
+
+        //declare an alternate location for your "WEB-INF/classes" dir:
+        File additionWebInfClasses = new File("build/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+
+    }
+
+}
---
+++ src/main/webapp/script.js
@@ -0,0 +1,21 @@
+// Request-ის გაგზავნა
+// async function register() {
+//     var url = webserverName + servletUrl + '?param1=param1Value' + '&param2=param2Value';
+//     var method = "POST" ან "GET"
+//     var response = await fetch(url, { method: "POST" });
+//
+//     // Response body-ს მიღება
+//     var body = await response.text();
+//
+//     // HTML ელემენტის დამატება/შეცვლა
+//     var div = document.getElementById("some-div-id");
+//     div.innerHTML = 'some html code here';
+// }
+
+
+
+
+
+
+
+
